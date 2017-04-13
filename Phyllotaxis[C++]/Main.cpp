ba55b3a942d02d353447e5150f3d4704bdc49f0d
7szczepanski <7szczepanski@gmail.com>
2017-04-13 18:21:19 +0200

    Some aestetics

--- Phyllotaxis[C++]/Main.cpp
+++ Phyllotaxis[C++]/Main.cpp
@@ -1,5 +1,5 @@
 /*
-PHYLLOTAXIS 
+PHYLLOTAXIS
 Created by Mateusz Szczepañski (7szczepanski) 2017 - 04 - 13
 */
 
@@ -7,10 +7,16 @@ Created by Mateusz Szczepañski (7szczepanski) 2017 - 04 - 13
 #include <math.h>
 #include <random>
 #include <time.h>
-//#include <iostream> // - for debugging 
+#include <iostream>
 
 using namespace std;
 using namespace sf;
+
+constexpr int width{ 200 }, height{ 200 };
+
+float Umap(float value, float istart, float istop, float ostart, float ostop) {
+	return ostart + (ostop - ostart)*((value - istart) / (istop - istart));
+}
 
 double unirand(float start, float end) {
 	std::random_device rd;
@@ -56,21 +62,34 @@ class Dot {
 	Vector2f pos;
 	Color color;
 	int size;
+
+
 public:
 	CircleShape dot;
-	Dot(float x, float y, int siz, Color col) {
+	int lifespan;
+	bool alive = true;
+	Dot(float x, float y, int siz) {
 		pos.x = x;
 		pos.y = y;
 		size = siz;
-		color = col;
+		lifespan = 855;
 	}
 	void setup() {
 		dot.setRadius(size);
-		dot.setFillColor(color);
 		dot.setPosition(pos);
+
+	}
+	void update() {
+		color = setColor(Umap(lifespan, 855, 1, 1, 360), 5, 127);
+		dot.setFillColor(color);
+		lifespan--;
 	}
 	void show(RenderWindow &target) {
 		target.draw(dot);
+
+	}
+	void checkLife() {
+		if (lifespan <= 0 || pos.x > width + 1 || pos.x<0 || pos.y>height || pos.y < 0) alive = false;
 	}
 };
 
@@ -81,13 +100,13 @@ public:
 int main()
 {
 	srand(time(NULL));
-	int n{ 0 }, c{ 6 };
+	int n{ 0 }, c{ 4 };
 	float a, r, x, y;
-	constexpr int width{ 800 }, height{ 800 };
+
 
 	sf::RenderWindow window(sf::VideoMode(width, height), "Phylotaxis");
-	sf::RectangleShape background(sf::Vector2f(width,height));
-	background.setFillColor(sf::Color(51,51,51));
+	sf::RectangleShape background(sf::Vector2f(width, height));
+	background.setFillColor(sf::Color(51, 51, 51));
 
 
 	vector<Dot> phyllotaxis;
@@ -100,23 +119,30 @@ int main()
 				window.close();
 		}
 
-		a = n*136.7;
+		a = n*137.5;
 		r = c*sqrt(n);
 		x = r * cos(a) + width / 2;
 		y = r * sin(a) + height / 2;
-		int randshitcolor = []()->int {return unirand(1, 360); }();
-		Dot dot(x, y, 5, (setColor(randshitcolor, 5, 127)));
+		int randshittycolor = []()->int {return unirand(1, 360); }();
+		Dot dot(x, y, 2);
 		dot.setup();
 		phyllotaxis.push_back(dot);
-		n += 2;
+		
+		n++;
 
 
 		window.clear();
 		window.draw(background);
-/*--------------------------------------------------------------| DRAW BELOW |----------------------------------------------------------------------------*/
+		/*--------------------------------------------------------------| DRAW BELOW |----------------------------------------------------------------------------*/
 
-		for (int i = 0; i < phyllotaxis.size(); i++) {
+		for (int i = phyllotaxis.size() - 1; i >= 0; i--) {
+			phyllotaxis[i].update();
+			phyllotaxis[i].checkLife();
 			phyllotaxis[i].show(window);
+
+
+			if (phyllotaxis[i].alive == false) { phyllotaxis.erase(phyllotaxis.begin() + i); }
+
 		}
 		window.display();
 	}
